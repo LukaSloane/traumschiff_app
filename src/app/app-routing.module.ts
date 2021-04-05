47711f5513f827f8cb2b2ef5626efdd629da497e
LukaSloane <luka.sloane@hotmail.com>
2021-04-05 17:10:51 +0200

    tabs nach home.html. Routing fehlerhaft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,39 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'ship-info',
-    loadChildren: () => import('./pages/ship-info/ship-info.module').then( m => m.ShipInfoPageModule)
-  },
-  {
-    path: 'ships',
-    loadChildren: () => import('./pages/ships/ships.module').then( m => m.ShipsPageModule)
-  },
-  {
-    path: 'livemap',
-    loadChildren: () => import('./pages/livemap/livemap.module').then( m => m.LivemapPageModule)
-  },
-  {
-    path: 'info',
-    loadChildren: () => import('./pages/info/info.module').then( m => m.InfoPageModule)
-  },
-  {
-    path: 'viewpoints',
-    loadChildren: () => import('./pages/viewpoints/viewpoints.module').then( m => m.ViewpointsPageModule)
-  },
-  {
-    path: 'todo',
-    loadChildren: () => import('./pages/todo/todo.module').then( m => m.TodoPageModule)
-  },
+  }
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,8 +4,83 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     component: HomePage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./home.module').then( m => m.HomePageModule)
+          }
+        ]
+      },
+      {
+        path: 'info',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/info/info.module').then( m => m.InfoPageModule)
+          }
+        ]
+      },
+      {
+        path: 'ship-info',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/ship-info/ship-info.module').then( m => m.ShipInfoPageModule)
+          }
+        ]
+      },
+      {
+        path: 'ships',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/ships/ships.module').then( m => m.ShipsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'livemap',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/livemap/livemap.module').then( m => m.LivemapPageModule)
+          }
+        ]
+      },
+      {
+        path: 'viewpoints',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/viewpoints/viewpoints.module').then( m => m.ViewpointsPageModule)
+          }
+        ]
+      },
+      {
+        path: 'todos',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/todo/todo.module').then( m => m.TodoPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
   }
 ];
 
